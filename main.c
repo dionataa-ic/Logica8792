a3ea4d00af0beed184f70a7dacdf0bcb85b6f3dd
dionataa-ic <dionataa@gmail.com>
2026-09-11 19:20:07 -0300

    Descobrindo qualé o par

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
 #include<stdio.h>
 #include<windows.h>
+#include<stdbool.h>
+
+bool ehPar(int numero){
+    return(numero % 2 == 0);
+}
 
 int main(){
+
+
 
     
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int a = 5;
-    int b = 4; 
+    bool resultado1 = ehPar(4);
+    bool resultado2 = ehPar(7);
+    bool resultado3 = ehPar(9);
+    bool resultado4 = ehPar(12);
+    bool resultado5 = ehPar(19);
 
-    printf("A soma é: %d\n", (a + b));
-    printf("A subtração é: %d\n", (a - b));
-    printf("A divisão é: %d\n", (a / b));
-    printf("A multiplicação é: %d\n", (a * b));
+    printf("4 é par? %d\n", resultado1);
+    printf("7 é par? %d\n", resultado2);
+    printf("9 é par? %d\n", resultado3);
+    printf("12 é par? %d\n", resultado4);
+    printf("19 é par? %d\n", resultado5);
+
 
     return 0;
+
+
 
 }
