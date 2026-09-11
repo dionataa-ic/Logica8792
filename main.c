730072a081c5c972f49493b3cb3291d2e89e0af2
dionataa-ic <dionataa@gmail.com>
2026-09-11 19:57:16 -0300

    Idade contando meses e dias

--- main.c
+++ main.c
@@ -1,34 +1,17 @@
 #include<stdio.h>
 #include<windows.h>
-#include<stdbool.h>
-
-bool ehPar(int numero){
-    return(numero % 2 == 0);
-}
 
 int main(){
 
+int idade;
 
+printf("Digite a idade: ");
+scanf("%d", &idade);
 
-    
-    SetConsoleCP(65001);
-    SetConsoleOutputCP(65001);
+printf("Equivale a %d meses\n", idade * 12);
+printf("Equivale a %d dias (aproximados)\n",idade * 365);
 
-    bool resultado1 = ehPar(4);
-    bool resultado2 = ehPar(7);
-    bool resultado3 = ehPar(9);
-    bool resultado4 = ehPar(12);
-    bool resultado5 = ehPar(19);
-
-    printf("4 é par? %d\n", resultado1);
-    printf("7 é par? %d\n", resultado2);
-    printf("9 é par? %d\n", resultado3);
-    printf("12 é par? %d\n", resultado4);
-    printf("19 é par? %d\n", resultado5);
-
-
-    return 0;
-
+return 0;
 
 
 }
