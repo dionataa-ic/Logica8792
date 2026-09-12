6802591bd10f29e38fb22afdb80c864fe3d79264
dionataa-ic <dionataa@gmail.com>
2026-09-11 21:23:56 -0300

    Descubrindo uma media apartir de notas

--- main.c
+++ main.c
@@ -11,14 +11,15 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    float raio;
-    const float PI = 3.14159;
+    float n1, n2, n3, media;
 
-    printf("Digite o raio do circulo: ");
-    scanf("%f", &raio);
+    printf("Digite três notas: ");
+    scanf("%f %f %f", &n1, &n2, &n3);
+    
 
-    printf("Area: %.2f\n", PI * raio * raio);
-    printf("Perimetro: %.2f\n", 2 * PI * raio);
+    media = (n1 + n2 + n3) / 3;
+
+    printf("Média: %.2f\n", media);
 
     
     return 0;
