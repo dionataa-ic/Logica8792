344e787f4b7b95761b88d2d6f2ee50567930df9c
dionataa-ic <dionataa@gmail.com>
2026-09-14 19:50:43 -0300

    voce é maior de idade?

--- main.c
+++ main.c
@@ -11,17 +11,17 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int n; 
-    
-    printf("Digite um número: ");
-    scanf("%d", &n);
+   int idade;
 
-  if(n % 2 == 0){
-    printf("\nPar");
-  }else{
-    printf("\nImpar");
-  }
+   printf("Digite uma idade: ");
+   scanf("%d", &idade);
 
+   if(idade >=18){
+    printf("Voce é maior de idade!");
+   }else{
+    printf("Voce é menos de idade!");
+   }
+   
     return 0;
 
 }
