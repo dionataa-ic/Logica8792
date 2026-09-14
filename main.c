a61e091368f3803efdc03ce1ceaadcfce5e4cdb9
dionataa-ic <dionataa@gmail.com>
2026-09-14 20:54:12 -0300

    Qual é o mais velho 4 opções

--- main.c
+++ main.c
@@ -11,16 +11,19 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a, b, c, maior;
+   int a, b, c, d, e,  maior;
 
    printf("Digite uma idade: ");
-   scanf("%d %d %d", &a, &b, &c);
+   scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
    
    maior = a;
 
 
    if(b > maior) maior = b;
    if(c > maior) maior = c;
+   if(d > maior) maior = d;
+   if(e > maior) maior = e;
+
 
    printf("Maior: %d", maior);
 
