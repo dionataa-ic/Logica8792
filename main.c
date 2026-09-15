fd85ee7e2d0d3040b6e76726d6899208da715e2d
dionataa-ic <dionataa@gmail.com>
2026-09-14 21:38:26 -0300

    comparação de numero maior e menos com resposta separada

--- main.c
+++ main.c
@@ -11,21 +11,41 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a, b, c, d, e,  maior;
+   int a, b, c, d, e,  menor, maior;
 
-   printf("Digite uma idade: ");
-   scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+   printf("Digite numero para a: ");
+   scanf("%d", &a);
+
+   printf("digite um numero para b: ");
+   scanf("%d", &b);
+
+   printf("digite um numero para c: ");
+   scanf("%d", &c);
+
+   printf("digite um numero para d: ");
+   scanf("%d", &d);
+
+   printf("digite um numero para e: ");
+   scanf("%d", &e);
    
-   maior = a;
+   menor = a;
+   maior = a; 
 
+   if(b < menor) menor = b;
+   if(c < menor) menor = c;
+   if(d < menor) menor = d;
+   if(e < menor) menor = e;
 
    if(b > maior) maior = b;
    if(c > maior) maior = c;
    if(d > maior) maior = d;
-   if(e > maior) maior = e;
+   if(e > maior) maior = e; 
 
+   printf("menor: %d\n", menor);
 
-   printf("Maior: %d", maior);
+   printf("maior: %d", maior);
+
 
     return 0;
 
