a6ea57242bda8f79c3a3ec7b194ee64815994e09
dionataa-ic <dionataa@gmail.com>
2026-09-14 21:44:52 -0300

    Comparando numeros

--- main.c
+++ main.c
@@ -11,41 +11,17 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-   int a, b, c, d, e,  menor, maior;
+    int a, b;
+
+    printf("Digite dois numeros: ");
+    scanf("%d %d", &a, &b);
 
 
-   printf("Digite numero para a: ");
-   scanf("%d", &a);
-
-   printf("digite um numero para b: ");
-   scanf("%d", &b);
-
-   printf("digite um numero para c: ");
-   scanf("%d", &c);
-
-   printf("digite um numero para d: ");
-   scanf("%d", &d);
-
-   printf("digite um numero para e: ");
-   scanf("%d", &e);
-   
-   menor = a;
-   maior = a; 
-
-   if(b < menor) menor = b;
-   if(c < menor) menor = c;
-   if(d < menor) menor = d;
-   if(e < menor) menor = e;
-
-   if(b > maior) maior = b;
-   if(c > maior) maior = c;
-   if(d > maior) maior = d;
-   if(e > maior) maior = e; 
-
-   printf("menor: %d\n", menor);
-
-   printf("maior: %d", maior);
-
+    if(a == b){
+        printf("Os Numeros sao iguais!\n");
+    }else{
+        printf("Os numeros sao diferentes!\n"); 
+    }
 
     return 0;
 
