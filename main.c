66b80b45312bb4e084df09996372181cede8114d
dionataa-ic <dionataa@gmail.com>
2026-09-16 20:57:22 -0300

    Dia Da Semana simplicado

--- main.c
+++ main.c
@@ -11,28 +11,38 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int diaDaSemana;
+    int dia;
+    
+    printf("Digite o número de 1 a 7: ");
+    scanf("%d", &dia);
 
-    printf("Digite um número de (1 a 7): ");
-    scanf("%d", &diaDaSemana);
 
-    if(diaDaSemana == 1){
-        printf("Domingo!");
-    }else if(diaDaSemana == 2){
-        printf("Segunda-Feira");
-    }else if(diaDaSemana == 3){
-        printf("Terça-feira");
-    }else if(diaDaSemana == 4){
-        printf("Quarta-Feira");
-    }else if("diaDaSemana == 5"){
-        printf("Quinta-feira");
-        }else if("diaDaSemana == 6"){
+    switch (dia){
+        case 1:
+            printf("Domingo!");
+            break;
+        case 2:
+            printf("Segunda-feira");
+            break;
+        case 3:
+            printf("Terça-feira");
+            break;
+        case 4:
+            printf("Quarta-feira");
+            break;
+        case 5:
+            printf("Quinta-feira");
+            break;
+        case 6:
             printf("Sexta-feira");
-        }else if("diaDaSemana ==7"){
-            printf("Sabádo");
-        }else{
-            printf("Valor inválido!");
-        }
+            break;
+        case 7:
+            printf("Sabado");
+            break;
+        default:
+            printf(" Número inválido!!");
+            break;
 
-        return 0;
+            return 0;
+    }
     }
