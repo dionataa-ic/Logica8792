9f2d99cc12fcc66615c7a5b60f9654ff2dae721c
dionataa-ic <dionataa@gmail.com>
2026-09-18 19:31:04 -0300

    Valor do Pedagio 2

--- main.c
+++ main.c
@@ -16,19 +16,36 @@ int main(){
     printf("--- SISTEMA DE PEDÁGIO ---\n");
     printf("1 - Moto\n");
     printf("2 - Carro Passeio\n");
+    printf("3 - Caminhão\n");
+    printf("4 - ônibus\n");
+    printf("5 - Caminhão Bi Trem\n");
+    printf("6 - Ambulancia\n");
     scanf("%d", &opcao);
 
     switch (opcao)
     {
     case 1: 
-        printf("Categoria: Moto | Tarifca: R$ 5,00\n");
+        printf("Categoria: Moto | Tarifa: R$ 5,00\n");
         break;
     case 2:
-        printf("Categoria: Carro Passeio | Tarifca R$ 10,00\n");
+        printf("Categoria: Carro Passeio | Tarifa R$ 10,00\n");
+        break;
+    case 3:
+        printf("Categoria: Caminhão | Tarifa: R$ 25,00\n");
+        break;  
+    case 4:
+        printf("Categoria: Ônibos | Tarifa: R$ 30,00\n");
+        break;
+    case 5:
+        printf("Categoria: Caminhão Bi Trem | Tarifa: R$ 55,00\n");
+        break;
+    case 6:
+        printf("Categoria: Ambulancia | Tarica: Isento");
         break;
     default:
         printf("Erro: Categoria não cadastrada no sitema.\n");
         break;
+
 
     }
 
