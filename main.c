9ba8482d1c907222372653e18e81c0744aabd0a9
dionataa-ic <dionataa@gmail.com>
2026-09-18 20:17:52 -0300

    Comanda para estações de ano 2

--- main.c
+++ main.c
@@ -14,62 +14,46 @@ int main(){
     int opcao;
 
         printf("--- ESTAÇÃO DO ANO---\n");
-        printf("1 - Dezembro Verão\n");
-        printf("2 - Janeiro Verão\n");
-        printf("3 - Março Verão\n");
-        printf("4 - Abril Outono\n");
-        printf("5 - Maio Outono\n");
-        printf("6 - Junho Outono\n");
-        printf("7 - julho Inverno\n");
-        printf("8 - agosto Inverno\n");
-        printf("9 - Julho Inverno\n");
-        printf("10 - Setembro Primavera\n");
-        printf("11 - Outubro Primavera\n");
-        printf("12 - Novembro Privamvera\n");
+        printf("12 - Dezembro Verão\n");
+        printf("1 - Janeiro Verão\n");
+        printf("2 - Março Verão\n");
+        printf("3 - Abril Outono\n");
+        printf("4 - Maio Outono\n");
+        printf("5 - Junho Outono\n");
+        printf("6 - julho Inverno\n");
+        printf("7 - agosto Inverno\n");
+        printf("8 - Julho Inverno\n");
+        printf("9 - Setembro Primavera\n");
+        printf("10 - Outubro Primavera\n");
+        printf("11 - Novembro Privamvera\n");
         scanf("%d", &opcao);
 
         switch (opcao)
         {
 
+    case 12:
     case 1:
-        printf("Qual estação: Verão\n");
-        break;
     case 2:
-        printf("Qual Estação: Verão\n");
-        break;
+            printf("Veraão\n");
+            break;
     case 3:
-            printf(" Qual Estação: Verão\n");
-            break;
     case 4:
-            printf(" Qual Estação: Outono\n");
-            break;
     case 5:
-            printf(" Qual Estação: Outono\n");
+            printf("Outono\n");
             break;
     case 6:
-            printf(" Qual Estação: Outono\n");
-            break;
     case 7:
-            printf(" Qual estação: Inverno\n");
-            break;
     case 8:
-            printf(" Qual Estação: Inverno\n");
+            printf("Inverno\n");
             break;
     case 9:
-            printf(" Qual estação: Inverno\n");
-            break;
     case 10:
-            printf(" Qual estação: Inverno\n");
-            break;
     case 11:
-            printf(" Qual estação: Primavera\n");
-            break;
-    case 12:
-            printf(" Qual estação: Primavera\n");
-            break;
+            printf("Primavera\n");
             default:
-            printf("Erro: Categoria não cadastrada no sistema.\n");
-            break;
+        printf("Erro: Categoria não cadastrada no sitema.\n");
+        break;
+
 
 
         }
