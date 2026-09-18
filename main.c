191ad916fafe4cbe6b930066c7547478565d9e47
dionataa-ic <dionataa@gmail.com>
2026-09-18 19:12:34 -0300

    Valor do Pedagio 1

--- main.c
+++ main.c
@@ -11,22 +11,26 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    float a, b, c;
-    
-    printf("Digite os tres lados do triângulo");
-    scanf("%f %f %f", &a, &b, &c);
+    int opcao;
 
+    printf("--- SISTEMA DE PEDÁGIO ---\n");
+    printf("1 - Moto\n");
+    printf("2 - Carro Passeio\n");
+    scanf("%d", &opcao);
 
-    if (a == b && b == c){
-        printf("Equilátero!\n");
-    }else if(a == b || a == c || b == c){
-        printf("Isosceles!\n");
-    }else{
-        printf("Escaleno!\n");
+    switch (opcao)
+    {
+    case 1: 
+        printf("Categoria: Moto | Tarifca: R$ 5,00\n");
+        break;
+    case 2:
+        printf("Categoria: Carro Passeio | Tarifca R$ 10,00\n");
+        break;
+    default:
+        printf("Erro: Categoria não cadastrada no sitema.\n");
+        break;
+
     }
-    
-
-    
 
     return 0;
     }
