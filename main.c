ca2785dccafdd967e44444225aa98803f0b6f2eb
dionataa-ic <dionataa@gmail.com>
2026-09-22 20:10:56 -0300

    Soma Usando void

--- main.c
+++ main.c
@@ -4,23 +4,30 @@
 #include<string.h>
 
 
-void saudar(const char *nome){
-    printf("Olá %s\n ", nome);
+int somar(int a, int b){
+    return a + b;
+} 
+void soma(){
+    int num1, num2;
+    printf("Digite um valor para num1: ");
+    scanf("%d", &num1);
+    printf("Digite um valor para num2: ");
+    scanf("%d", &num2);
+    int resultado1 = num1 + num2;
+    printf(" O resultado da soma com void é %d\n", resultado1);
 }
-
-    
 int main(){
 
-
+int num1, num2; 
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    saudar("Dionata Ivan");
-    saudar("Correa");
-    saudar("De Souza");
+    // int resultado = somar (5, 3);
+    // printf("O resultado da soma é: %d", resultado);
 
-    
+    soma(num1, num2);
+
   
     return 0;
     }
