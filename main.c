5fc6c4a72e4c44d8d4371a6d5c01f72ed528acad
dionataa-ic <dionataa@gmail.com>
2026-09-23 20:12:59 -0300

    Tabuada do 7 (2)

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 #include<math.h>
 
 
-    void tabuada(int n){
-        printf("Tavuada do %d\n", n);
+    int tabuada(int n){
+        int resultado; 
+        printf("Tabuada do %d:\n", n);
         for(int i = 1; i <= 10; i++){
-            printf("%d x %d = %d\n", n, i, n * i);
-
+            resultado = n * i;
+            printf("%d x %d = %d\n", n, i, resultado);
         }
-    }
+        return resultado; 
+        }
 
 int main(){
 
@@ -22,7 +24,8 @@ int main(){
 
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    tabuada(7);
+    int ultimo = tabuada(7);
+    printf("Último valor da tabuada: %d\n", ultimo);
 
 
     
