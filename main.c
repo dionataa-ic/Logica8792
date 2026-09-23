9fdd04ff1f049c8b5a488309ef3978aede8b6ad6
dionataa-ic <dionataa@gmail.com>
2026-09-22 21:13:44 -0300

    1 ao 5 for

--- main.c
+++ main.c
@@ -3,31 +3,21 @@
 #include<locale.h>
 #include<string.h>
 
-
-int somar(int a, int b){
-    return a + b;
-} 
-void soma(){
-    int num1, num2;
-    printf("Digite um valor para num1: ");
-    scanf("%d", &num1);
-    printf("Digite um valor para num2: ");
-    scanf("%d", &num2);
-    int resultado1 = num1 + num2;
-    printf(" O resultado da soma com void é %d\n", resultado1);
-}
 int main(){
 
-int num1, num2; 
+
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    // int resultado = somar (5, 3);
-    // printf("O resultado da soma é: %d", resultado);
+    int num[5] = {1, 2, 3, 4, 5};
 
-    soma(num1, num2);
+    for(int i = 0; i < 5; i++){
+        printf("%d\n", num[i]);
+    }
 
-  
+
+
+   
     return 0;
     }
