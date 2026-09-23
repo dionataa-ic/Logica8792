fc64d0dc52049a0781ed5b32935a83b14511d0b3
dionataa-ic <dionataa@gmail.com>
2026-09-22 21:40:43 -0300

    nomess

--- main.c
+++ main.c
@@ -10,24 +10,15 @@ int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    int horas[12];
-    int minutos[60];
+    char nomes [3][20] ={
+        "Dionata",
+        "Jessica",
+        "Correa"
+    };
 
-    for(int i = 0; i < 12; i++){
-        horas[i] = i + 1;
+    for(int i = 0; i < 3; i++){
+        printf("%s\n", nomes[i]);
     }
-
-    for(int j = 0; j < 60; j++){
-        minutos[j] = j;
-    }
-
-    //simulhar relógio
-    for( int i = 0; i < 12; i++){
-        for(int j = 0; j < 60; j++){
-            printf("Hora: %2d:%02d\n", horas[i], minutos[j]);
-        }
-    }
-
-   
+    
     return 0;
     }
